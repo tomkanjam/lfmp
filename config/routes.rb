ff15fe9f0ef35f0065d4ca0cfd1ae418db785ecb
root <root@rails.(none)>
2011-01-14 02:35:09 +0000

    trying to get find_or_create working for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :pages
+
+  map.resources :playlists
+  map.resources :users
+  map.root :controller => 'users', :action => 'new'
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
